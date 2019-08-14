e4701d605b94da154b8d3c1d1b21e004e449c9db
DavisVaughan <davis@rstudio.com>
2019-08-13 20:56:39 -0400

    Include guards

--- src/cshared.h
+++ src/cshared.h
@@ -1,3 +1,8 @@
+#ifndef CSHARED_H
+#define CSHARED_H
+
 #define R_NO_REMAP
 #include <R.h>
 #include <Rinternals.h>
+
+#endif
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,11 @@
+#ifndef CSHARED_UTILS_H
+#define CSHARED_UTILS_H
+
 #include "cshared.h"
 
 // We are going to be calling this object from other `.c` files.
 // To "share" it with them, we create this `utils.h` header file,
 // and include the definition of our shared empty integer
 SEXP cshared_shared_empty_int;
+
+#endif
